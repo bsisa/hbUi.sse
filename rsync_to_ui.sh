4d9f01ef77054f22916b97ccfa64df906091e180
refond <refon@pobox.com>
2016-09-12 12:47:01 +0200

    Move to HyperBird Javascript projects standard layout modeled after standard Maven / SBT layouts.

--- rsync_to_ui.sh
+++ rsync_to_ui.sh
@@ -3,7 +3,7 @@
 # Simple rsync from hbUi.sse/dist/... to a given target project lib for quick dev turnaround
 #
 export HBUI_SSE_HOME=/home/patrick/Documents/hb/5/workspace/hbUi.sse/
-export HBUI_SSE_DIST=${HBUI_SSE_HOME}/main/dist/
+export HBUI_SSE_DIST=${HBUI_SSE_HOME}/target/dist/
 export HBUI_SSE_TARGET=/home/patrick/Documents/hb/5/workspace/hb-ui/main/lib/hbUi.sse/
 cd ${HBUI_SSE_HOME}/main/
 grunt
